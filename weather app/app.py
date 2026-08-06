204c6d7c00a19850e7797845624fe620c97612ab
Kai <kaijbooth@gmail.com>
2026-08-06 21:07:50 +0100

    Update app.py

--- weather app/app.py
+++ weather app/app.py
@@ -2,10 +2,14 @@
 
 from tkinter import *
 
+# Configs
 window = Tk()
 window.title("What Weather")
 window.geometry("420x420")
 window.mainloop(). # place window on screen
+
+icon = Photoimage(file="sample.png")
+
 
 
 
