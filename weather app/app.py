f43fdd2c57a2465fdf924ef53a5af2d05e5e7be2
Kai <kaijbooth@gmail.com>
2026-08-07 14:05:40 +0100

    Enhance GUI with new background and title label
    Updated window size, background color, and added a label for the title.

--- weather app/app.py
+++ weather app/app.py
@@ -1,15 +1,27 @@
 # using tkinter to create interface, that can be linked to backend
 
+from pathlib import Path
 from tkinter import *
 
-# Configs
 window = Tk()
+
+
 window.title("What Weather")
-window.geometry("420x420")
-window.config(background="")
+window.config(background="#242424")
+window.geometry("1000x1000")
+
+
+icon = PhotoImage(file="images.png")
+window.iconphoto(True, icon)
+
+label = Label(window,text="What Weather", font=("Times New Roman",40,"bold"),fg="#00ff00", bg="black")
+label.pack()
+label.place(y=10)
+
+
+
+
+
+
 
 window.mainloop() # place window on screen
-
-
-icon = PhotoImage(file='images.png')
-window.iconphoto(True,icon)
