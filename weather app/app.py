2437cc1bf396c90e31216d04e6b1259ad45e9e7e
Kai <kaijbooth@gmail.com>
2026-08-07 14:10:04 +0100

    Update app.py
    Updated label properties for better appearance.

--- weather app/app.py
+++ weather app/app.py
@@ -14,9 +14,17 @@ window.geometry("1000x1000")
 icon = PhotoImage(file="images.png")
 window.iconphoto(True, icon)
 
-label = Label(window,text="What Weather", font=("Times New Roman",40,"bold"),fg="#00ff00", bg="black")
+label = Label(window,
+            text="What Weather",
+            font=("Arial",40,"bold"),
+            fg="#00ff00", 
+            bg="black",
+            relief=RAISED,
+            bd=15,
+            padx=20,
+            pady=20)
 label.pack()
-label.place(y=10)
+
 
 
 
